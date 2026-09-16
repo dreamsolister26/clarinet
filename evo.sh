e3f4a7845aebd4c1afeea7d5a9d24561cca0462f
Yoshikawa Yuuko <oumaekumiko123@gmail.com>
2026-09-17 06:14:33 +0700

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # repo init
-repo init -u https://github.com/sweet-bullet/evolution_manifest.git -b cnb --git-lfs --depth=1
+repo init -u https://github.com/LineageOS/android.git -b lineage-24.0 --git-lfs --depth=1
 
 # sync + remove dirty
 /opt/crave/resync.sh
 repo sync -c --force-sync --force-remove-dirty --no-tags --no-clone-bundle # For fixing sync error
 
 # device source
-git clone https://github.com/MinamiQuartet/android_device_xiaomi_earth.git -b EvolutionX-17 device/xiaomi/earth --depth=1
+git clone https://github.com/MinamiQuartet/android_device_xiaomi_earth.git -b lineage-24.0 device/xiaomi/earth
+git clone https://github.com/MinamiQuartet/vendor_xiaomi_earth.git -b lineage-24.0-ims vendor/xiaomi/earth --depth=1
+git clone https://github.com/MinamiQuartet/android_kernel_xiaomi_earth.git -b lineage-24.0 kernel/xiaomi/earth --depth=1
+git clone https://github.com/MinamiQuartet/minami-sign.git -b keys vendor/lineage-priv/keys
+
+# Hardware Repos
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-24.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
 
 # build start
 . build/envsetup.sh
@@ -19,7 +27,7 @@ export SOONG_NINJA=ninja
 
 # start build
 lunch lineage_earth-cp2a-userdebug
-m evolution
+mka bacon
 
 # Upload files to gofile
 echo "Upload to gofile will be started..."
