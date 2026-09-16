bdb9e80a7e1c8d36893b60938bcfb4cb79e43d17
Yoshikawa Yuuko <oumaekumiko123@gmail.com>
2026-09-16 21:45:38 +0700

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -2,11 +2,13 @@
 
 # repo init
 repo init -u https://github.com/sweet-bullet/evolution_manifest.git -b cnb --git-lfs --depth=1
-/opt/crave/resync.sh # sync source
+
+# sync + remove dirty
+/opt/crave/resync.sh
+repo sync -c --force-sync --force-remove-dirty --no-tags --no-clone-bundle # For fixing sync error
 
 # device source
-rm -rf device/xiaomi/earth
-git clone https://github.com/HiroZukki/device_xiaomi_earth.git -b EvolutionX-17 device/xiaomi/earth --depth=1
+git clone https://github.com/MinamiQuartet/android_device_xiaomi_earth.git -b EvolutionX-17 device/xiaomi/earth --depth=1
 
 # build start
 . build/envsetup.sh
@@ -17,7 +19,6 @@ export SOONG_NINJA=ninja
 
 # start build
 lunch lineage_earth-cp2a-userdebug
-make installclean
 m evolution
 
 # Upload files to gofile
