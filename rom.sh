5682d1cd38a0a4b1ed56f92f3dadfdb9df00e813
Yoshikawa Yuuko <oumaekumiko123@gmail.com>
2026-09-17 15:44:02 +0700

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-# init & sync
-repo init -u https://github.com/sweet-bullet/evolution_manifest.git -b cnb --git-lfs --depth=1
+# init
+repo init -u https://github.com/Pixelify-AOSP/platform_manifest.git -b 17 --git-lfs --depth=1
+
+# Sync + remove dirty
 /opt/crave/resync.sh
 repo sync -c --force-sync --force-remove-dirty --no-tags --no-clone-bundle # For fixing sync error
 
 # device source
-git clone https://github.com/MinamiQuartet/android_device_xiaomi_earth.git -b EvolutionX-17 device/xiaomi/earth
+git clone https://github.com/MinamiQuartet/android_device_xiaomi_earth.git -b ASCP-17 device/xiaomi/earth
+
+# Patching build soong
+cd build/soong
+wget 
+patch -p1 < soong.patch && rm -f soong.patch
+cd ../..
 
 # Setup build
 . build/envsetup.sh
@@ -16,16 +24,18 @@ export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 # start build
-lunch lineage_earth-cp2a-userdebug 
+lunch earth-cp2a-userdebug 
 mka bacon
 
 # Upload
 echo "upload to gofile..."
 if [ -f out/target/product/earth/*202608*.zip ]; then
     wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-    chmod +x upload.sh ; ./upload.sh out/target/product/earth/PixelOS_*.zip
-    echo "upload done!"
+    chmod +x upload.sh && ./upload.sh out/target/product/earth/PixelOS_*.zip
+    cd build/soong && git restore . && cd ../..
+    echo "upload & clean up done!"
 else
+    cd build/soong && git restore . && cd ../..
     echo "no zip found at out/ dir..."
     exit 1
 fi
