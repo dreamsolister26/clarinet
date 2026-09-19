6a6ec70f754b822fa4127e6806ac32b71d15a394
Yoshikawa Yuuko <oumaekumiko123@gmail.com>
2026-09-19 23:28:00 +0700

    Modify evo.sh for EvolutionX build process
    Updated repository sources and build commands for EvolutionX.

--- evo.sh
+++ evo.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
+# remove device source
+rm -rf device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
+rm -rf hardware/xiaomi hardware/mediatek device/mediatek/sepolicy_vndr
+
 # repo init
-repo init -u https://github.com/LineageOS/android.git -b lineage-24.0 --git-lfs --depth=1
+repo init -u https://github.com/sweet-bullet/evolution_manifest.git -b cnb --git-lfs --depth=1
 
 # sync + remove dirty
 /opt/crave/resync.sh
 repo sync -c --force-sync --force-remove-dirty --no-tags --no-clone-bundle # For fixing sync error
 
-# device source
-git clone https://github.com/MinamiQuartet/android_device_xiaomi_earth.git -b lineage-24.0 device/xiaomi/earth
-git clone https://github.com/MinamiQuartet/vendor_xiaomi_earth.git -b lineage-24.0-ims vendor/xiaomi/earth --depth=1
-git clone https://github.com/MinamiQuartet/android_kernel_xiaomi_earth.git -b lineage-24.0 kernel/xiaomi/earth --depth=1
-
-# Hardware Repos
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-24.0 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
-
-# Patching build/soong
-cd build/soong
-wget https://raw.githubusercontent.com/dreamsolister26/clarinet/refs/heads/main/soong.patch
-patch -p1 < soong.patch && rm -f soong.patch
-cd ../..
+# device source 
+git clone https://github.com/MinamiQuartet/android_device_xiaomi_earth.git -b EvolutionX-17 device/xiaomi/earth
 
 # build start
 . build/envsetup.sh
@@ -32,17 +23,16 @@ export SOONG_NINJA=ninja
 
 # start build
 lunch lineage_earth-cp2a-userdebug
-mka bacon
+make installclean
+m evolution
 
 # Upload files to gofile
 echo "Upload to gofile will be started..."
 if [ -f out/target/product/earth/*202609*.zip ]; then
     wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
     chmod +x upload.sh && ./upload.sh out/target/product/earth/*202609*.zip
-    cd build/soong && git restore . && cd ../..
-    echo "Upload & clean up Done!"
+    echo "Upload Done!"
 else
-    cd build/soong && git restore . && cd ../..
     echo "No zip found in out/ dir!" 
     exit 1
 fi
