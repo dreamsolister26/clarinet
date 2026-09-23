174b6da40b2dd46080a32223f78df34b6032b9a7
Yoshikawa Yuuko <oumaekumiko123@gmail.com>
2026-09-23 22:27:09 +0700

    Update hame.sh

--- hame.sh
+++ hame.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
 # repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 17 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs --depth=1
 
 # sync + remove dirty
 /opt/crave/resync.sh
-repo sync -c --force-sync --force-remove-dirty --no-tags --no-clone-bundle # For fixing sync error
 
 # device source 
-git clone https://github.com/MinamiQuartet/android_device_xiaomi_earth.git -b Infinity-17 device/xiaomi/earth
+git clone https://github.com/MinamiQuartet/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
 
 # patching build/soong
-cd build/soong
-wget https://raw.githubusercontent.com/dreamsolister26/clarinet/refs/heads/main/soong.patch
-patch -p1 < soong.patch && rm -f soong.patch
+cd vendor/lineage
+curl -LSs "https://github.com/dreamsolister26/vendor_lunaris/commit/5f52fddccb4036e113ace9de8ae5437d225f999c.patch" -o vendor.patch
+git am vendor.patch && rm -f vendor.patch
 cd ../..
 
-# setup env
-. build/envsetup.sh
-
-# export
 export BUILD_USERNAME=yuuko
 export BUILD_HOSTNAME=minami
-export SOONG_NINJA=ninja
 
-# start build !
-lunch infinity_earth-userdebug
+# setup env & build
+. build/envsetup.sh
+lunch lineage_earth-bp4a-userdebug
 mka bacon
 
 # Upload files to gofile
@@ -33,10 +28,8 @@ echo "Upload to gofile will be started..."
 if [ -f out/target/product/earth/*2026*.zip ]; then
     wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
     chmod +x upload.sh && ./upload.sh out/target/product/earth/boot.img && ./upload.sh out/target/product/earth/*2026*.zip
-    cd build/soong && git restore . && cd ../.. && rm -f upload.sh
     echo "Upload Done!"
 else
-    cd build/soong && git restore . && cd ../..
     echo "No zip found in out/ dir!" 
     exit 1
 fi
