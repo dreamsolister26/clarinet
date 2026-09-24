aea6617f2aacc53fd9a6d807054e9b6915064f19
Yoshikawa Yuuko <oumaekumiko123@gmail.com>
2026-09-24 12:20:49 +0700

    Update kernul.sh

--- kernul.sh
+++ kernul.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git config --global user.name "dreamsolister26"
+git config --global user.email "oumaekumiko123@gmail.com"
+
 # kernel
 git clone --depth=1 https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-24.0 kernel
 cd kernel && git checkout -b lineage-24.0-new
